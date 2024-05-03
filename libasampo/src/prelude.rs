f5766195ed1fd657ea1a29b0f9d95dbc294a63df
Mikael Forsberg <mkforsb@gmail.com>
2024-05-03 07:52:09 +0200

    feat: update prelude

--- libasampo/src/prelude.rs
+++ libasampo/src/prelude.rs
@@ -3,4 +3,5 @@
 // Copyright (c) 2024 Mikael Forsberg (github.com/mkforsb)
 
 pub use crate::samples::SampleOps;
-pub use crate::sources::{SourceReaderOps, SourceOps};
+pub use crate::samplesets::{SampleSetLabellingOps, SampleSetOps};
+pub use crate::sources::{SourceOps, SourceReaderOps};
