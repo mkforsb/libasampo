b6837eb0a5c55d748fb1daaca15622b3bf1598b3
Mikael Forsberg <mkforsb@gmail.com>
2024-05-03 15:19:15 +0200

    feat(serialize): remove (de)serialize shorthand

--- libasampo/tests/serialize.rs
+++ libasampo/tests/serialize.rs
@@ -4,13 +4,13 @@
 
 use libasampo::{
     samples::{BaseSample, Sample, SampleMetadata, SampleURI},
-    serialize::{deserialize, serialize},
+    serialize::{self, TryFromDomain, TryIntoDomain},
     sources::{file_system_source::FilesystemSource, Source},
 };
 use uuid::Uuid;
 
 #[test]
-fn test_serialize_sample() {
+fn test_ser_de_sample() {
     let source_uuid = Uuid::new_v4();
 
     let sample = Sample::BaseSample(BaseSample::new(
@@ -24,17 +24,23 @@ fn test_serialize_sample() {
         Some(source_uuid),
     ));
 
-    let x = serialize(&sample).unwrap();
-    assert_eq!(deserialize::<Sample>(x).unwrap(), sample);
+    let serdeable = serialize::Sample::try_from_domain(&sample).unwrap();
+    let encoded = serde_json::to_string(&serdeable).unwrap();
+    let decoded = serde_json::from_str::<serialize::Sample>(&encoded).unwrap();
+    let deserialized = decoded.try_into_domain().unwrap();
+    assert_eq!(deserialized, sample);
 }
 
 #[test]
-fn test_serialize_fs_source() {
+fn test_ser_de_fs_source() {
     let source = Source::FilesystemSource(FilesystemSource::new(
         "/tmp".to_string(),
         vec!["wav".to_string(), "ogg".to_string()],
     ));
 
-    let x = serialize(&source).unwrap();
-    assert_eq!(deserialize::<Source>(x).unwrap(), source);
+    let serdeable = serialize::Source::try_from_domain(&source).unwrap();
+    let encoded = serde_json::to_string(&serdeable).unwrap();
+    let decoded = serde_json::from_str::<serialize::Source>(&encoded).unwrap();
+    let deserialized = decoded.try_into_domain().unwrap();
+    assert_eq!(deserialized, source);
 }
